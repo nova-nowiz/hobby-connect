10bf67bff7ac3f55b28a49ce6e3d084055b43d51
Titdrax <tristan.bonneau@efrei.net>
2021-03-31 20:49:38 +0200

    Added apollo dependency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { GraphQLModule } from './graphql.module';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+import { ThemesComponent } from './themes/themes.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ThemesComponent],
+  imports: [BrowserModule, AppRoutingModule, GraphQLModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/graphql.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { APOLLO_OPTIONS } from 'apollo-angular';
+import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
+import { HttpLink } from 'apollo-angular/http';
+import { HttpHeaders } from '@angular/common/http';
+import { environment } from '../environments/environment';
+
+const uri = 'https://hobby-connect.herokuapp.com/v1/graphql';
+export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
+  return {
+    link: httpLink.create({
+      uri,
+      headers: new HttpHeaders({
+        'content-type': 'application/json',
+        'x-hasura-admin-secret': environment.HASURA_KEY,
+      }),
+    }),
+    cache: new InMemoryCache(),
+  };
+}
+
+@NgModule({
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: createApollo,
+      deps: [HttpLink],
+    },
+  ],
+})
+export class GraphQLModule {}

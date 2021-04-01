493ea5e097aa3279c0c5fffc68dedd1530d0a298
Levasseur <sebastien.levasseur@efrei.net>
2021-04-01 18:38:49 +0200

    first version of responsive mat-card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { ThemeListComponent } from './theme-list/theme-list.component';
 import { ThemeItemComponent } from './theme-item/theme-item.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -15,7 +18,10 @@ import { ThemeItemComponent } from './theme-item/theme-item.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
